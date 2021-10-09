cd4309d611e5defca73d6aeb6a50f2684da7df9e
rh7321 <85012437+rh7321@users.noreply.github.com>
2021-10-09 23:34:06 +0900

    Update movieP.php

--- admin/common/page/movie/movieP.php
+++ admin/common/page/movie/movieP.php
@@ -9,13 +9,15 @@
     $Y_Release  = $_POST["inputRelease"];
     $Y_Director   = $_POST["inputDirector"];
     $Y_Actor   = $_POST["inputActor"];
-
+    echo $Y_Code;
     // M_Picture_Code,
     if(!$conn){
         echo "DB연결 실패!";
-    }else{ //'$Y_File', 
-    $sql = "INSERT INTO movie_info(M_Code,  M_Title, E_M_Title, M_Genre_Code, M_Rel_Date, 
+    }else{ 
+        
+    $sql = "INSERT INTO movie_info(M_Code,  M_Title, E_M_Title, M_Genre_Code,
     M_Summary, Dircetor_code, Actor_Code) VALUES ('$Y_Code', '$Y_Title', '$Y_Title_en', '$Y_Genre', '$Y_Release', '$Y_Director', '$Y_Actor')";
+    echo $sql;
     $result = mysqli_query($conn, $sql);
 }
 ?>
