09b121ae501cf941bfa8f34baf74d7112f9a8731
AhreumKim <ahreum.kim38@gmail.com>
2021-10-15 11:11:13 +0900

    theater_infoDB 수정

--- theater_infoDB/cgv_theater_info.sql
+++ theater_infoDB/cgv_theater_info.sql
@@ -8,15 +8,16 @@ CREATE TABLE theater_info (
         theater_name varchar(20) not null,
         theater_zipcode varchar(10) not null,
         theater_addr1 varchar(100) not null,
-        theater_addr2 varchar(100) not null,
-        theater_addr3 varchar(100) not null,
+        theater_addr2 varchar(100),
+        theater_addr3 varchar(100),
         theater_tel varchar(20) not null
-    ) default character set utf8 collate utf8_general_ci;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
     
     select * from theater_info;
     
+    
+    
     DESC theater_info;
-    ALTER TABLE theater_info DROP theater_zipcode;
-    ALTER TABLE theater_info ADD theater_zipcode varchar(10) not null;
+    
    
     
