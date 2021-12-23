4f646a55509ed4fdb9099c73b9b4fb77e942e43a
rh7321 <85012437+rh7321@users.noreply.github.com>
2021-12-23 22:29:44 +0900

    Update cgv_time_info0.sql

--- theater_infoDB/cgv_time_info0.sql
+++ theater_infoDB/cgv_time_info0.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `cgv` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cgv`;
 -- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
 --
 -- Host: localhost    Database: cgv
@@ -54,4 +56,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-23 22:25:55
+-- Dump completed on 2021-12-23 22:29:36
